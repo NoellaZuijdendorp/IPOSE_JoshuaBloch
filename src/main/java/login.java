1f6e55984691de2af0b0e91c2fe3c51fd8c5fef1
NoellaZuijdendorp <s1142232@student.hsleiden.nl>
2023-04-04 20:32:53 +0200

    new game button, en klein beetje styling

--- src/main/java/login.java
+++ src/main/java/login.java
@@ -1,11 +1,16 @@
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -13,24 +18,33 @@ import java.io.FileNotFoundException;
 public class login extends Application {
 
         public void start(Stage login) throws FileNotFoundException {
-            FileInputStream inputstream = new FileInputStream("C:\\Users\\nozu2\\OneDrive - Hogeschool Leiden\\Leerjaar 1\\Periode 3\\IPOSE\\HSLEIDENTALE.png");
-            Image image = new Image(inputstream);
             login.setTitle("HSLEIDENTALE");
 
-            Button buttonLogin = new Button("New Game");
-            StackPane schermLogin = new StackPane();
+            Button buttonLogin_newgame = new Button("New Game");
+            Button buttonLogin_highscore = new Button("Highscore");
+            GridPane schermLogin = new GridPane();
 
-            schermLogin.getChildren().add(buttonLogin);
-            Scene sc = new Scene(schermLogin, 200, 200);
+            schermLogin.setHgap(5);
+            schermLogin.setVgap(5);
+            schermLogin.add(buttonLogin_highscore, 0, 3);
+            schermLogin.add(buttonLogin_newgame, 0, 2);
+
+            Scene sc = new Scene(schermLogin, 800, 500);
+
+            Image img = new Image("C:\\Users\\nozu2\\OneDrive - Hogeschool Leiden\\Leerjaar 1\\Periode 3\\IPOSE\\HSLEIDENTALE_white.png");
+            schermLogin.setStyle("-fx-background-image: " + img);
+//            schermLogin.setStyle("-fx-background-color: black;");
+
 
             login.setScene(sc);
             login.show();
 
         }
 
-        public static void main(String args[])
-        {
-            // launch the application
-            launch(args);
-        }
+    public static void main(String args[])
+    {
+        // launch the application
+        launch(args);
     }
+
+}
