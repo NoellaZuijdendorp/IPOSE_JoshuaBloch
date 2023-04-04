37d4da65529a1bc5c5a5ae28c20020e7eb4adb60
NoellaZuijdendorp <s1142232@student.hsleiden.nl>
2023-04-04 16:29:47 +0200

    button login, nu helemaal werkend

--- src/main/java/login.java
+++ src/main/java/login.java
@@ -1,28 +1,31 @@
+import com.almasb.fxgl.dsl.FXGL;
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class login extends Application {
 
-        public void start(Stage login)
-        {
+        public void start(Stage login) throws FileNotFoundException {
+            FileInputStream inputstream = new FileInputStream("C:\\Users\\nozu2\\OneDrive - Hogeschool Leiden\\Leerjaar 1\\Periode 3\\IPOSE\\HSLEIDENTALE.png");
+            Image image = new Image(inputstream);
             login.setTitle("HSLEIDENTALE");
 
-            Button button = new Button("button");
+            Button buttonLogin = new Button("New Game");
+            StackPane schermLogin = new StackPane();
 
-            StackPane scherm = new StackPane();
+            schermLogin.getChildren().add(buttonLogin);
+            Scene sc = new Scene(schermLogin, 200, 200);
 
-            scherm.getChildren().add(button);
-
-            Scene sc = new Scene(button, 200, 200);
-
-            // set the scene
             login.setScene(sc);
-
             login.show();
+
         }
 
         public static void main(String args[])
