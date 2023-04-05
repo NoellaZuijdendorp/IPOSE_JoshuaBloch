d281020fa93f88519b92c0a53a9ebd0ce756beeb
Niozk <99679240+Niozk@users.noreply.github.com>
2023-04-06 00:00:14 +0200

    projectile move and speed ge-add
    -ook muziek toegevoegd

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,17 +2,22 @@ import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
-import com.almasb.fxgl.physics.CollisionHandler;
+import javafx.geometry.Point2D;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import java.util.Map;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.io.File;
 
 public class Game extends GameApplication {
     private final int gameWidth = 800;
     private final int gameHeight = 800;
     private final String gameTitle = "HSLEIDENTALE";
     private final String gameVersion = "1.0";
+    private MediaPlayer mediaPlayer;
     private Entity player;
     private Player playerCreator = new Player(3, 0.25, 0.25);
     private Entity boss;
@@ -30,12 +35,17 @@ public class Game extends GameApplication {
 
     @Override
     protected void initGame(){
+        String musicFile = "Mortal_Kombat.mp3"; //TODO: KIJK OF DIE LOOPT en fix geluid lager
+        Media sound = new Media(new File(musicFile).toURI().toString());
+        mediaPlayer = new MediaPlayer(sound);
+        mediaPlayer.play();
+
         player = playerCreator.createEntity();
 
         bossCreator = new Boss(0.75, 0.75);
         boss = bossCreator.createEntity();
 
-        testProjectileCreator = new Projectile(1, 1, 2, 2);
+        testProjectileCreator = new Projectile(1, 1, 1, 2, 2);
         testProjectile = testProjectileCreator.createEntity();
     }
 
@@ -62,13 +72,10 @@ public class Game extends GameApplication {
 
      @Override
      protected void initPhysics(){
-        FXGL.getPhysicsWorld().addCollisionHandler(new CollisionHandler(EntityTypes.PLAYER, EntityTypes.PROJECTILE) {
-            @Override
-            protected void onCollision(Entity player, Entity bossProjectile) {
-                FXGL.inc("hp", playerCreator.getsDamaged(testProjectileCreator));
-                playerCreator.getHP().death(player);
-            }
-        });
+         FXGL.onCollision(EntityTypes.PLAYER, EntityTypes.PROJECTILE, (player, projectile) -> {
+             FXGL.inc("hp", playerCreator.getsDamaged(testProjectileCreator));
+             playerCreator.getHP().death(player);
+         });
      }
 
      @Override
@@ -84,6 +91,13 @@ public class Game extends GameApplication {
      protected void initGameVars(Map<String, Object> vars){
         vars.put("hp", playerCreator.getHP().getCurrentHP());
      }
+
+    @Override
+    protected void onUpdate(double tpf) {
+        Point2D velocity = testProjectile.getObject("velocity");
+        testProjectile.translate(velocity);
+
+    }
 
     public static void main(String[] args){
         launch(args);
--- src/main/java/Projectile.java
+++ src/main/java/Projectile.java
@@ -1,19 +1,22 @@
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.components.CollidableComponent;
+import javafx.geometry.Point2D;
 
 public class Projectile {
     private Entity entity;
     private double sizeScaleX;
     private double sizeScaleY;
     private int damage;
-    private int speed;
+    private int speedX;
+    private int speedY;
 
-    public Projectile(double sizeScaleX, double sizeScaleY, int damage, int speed) {
+    public Projectile(double sizeScaleX, double sizeScaleY, int damage, int speedX, int speedY) {
         this.sizeScaleX = sizeScaleX;
         this.sizeScaleY = sizeScaleY;
         this.damage = damage;
-        this.speed = speed;
+        this.speedX = speedX;
+        this.speedY = speedY;
     }
 
     public Entity createEntity() { //TODO: VERANDER ALLES IN DEZE FUNCTIE
@@ -22,6 +25,7 @@ public class Projectile {
                 .viewWithBBox("")
                 .scale(sizeScaleX, sizeScaleY)
                 .with(new CollidableComponent(true))
+                .with("velocity", new Point2D(speedX, speedY))
                 .type(EntityTypes.PROJECTILE)
                 .buildAndAttach();
         return entity;
@@ -29,9 +33,5 @@ public class Projectile {
 
     public int getDamage() {
         return damage;
-    }
-
-    public int getSpeed() {
-        return speed;
     }
 }
