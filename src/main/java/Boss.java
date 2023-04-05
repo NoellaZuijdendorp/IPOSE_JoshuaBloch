7bd112d32064804bca0aced5b9d66da0f15cbb49
Niozk <99679240+Niozk@users.noreply.github.com>
2023-04-05 17:14:39 +0200

    Big update
    -Soort van death screen
    -Abstraction toegepast
    -Veel bugfixes en refactors

--- src/main/java/Boss.java
+++ src/main/java/Boss.java
@@ -4,29 +4,20 @@ import com.almasb.fxgl.entity.components.CollidableComponent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
-public class Boss {
-    private Entity entityBuilder;
-    private HP hp;
-    private double sizeScaleX;
-    private double sizeScaleY;
+public class Boss extends Character {
 
-    public Boss(int maxHP ,double sizeScaleX, double sizeScaleY) {
-        this.hp = new HP(maxHP);
+    public Boss(double sizeScaleX, double sizeScaleY) {
         this.sizeScaleX = sizeScaleX;
         this.sizeScaleY = sizeScaleY;
     }
 
-    public Entity createBoss() {
-        entityBuilder = FXGL.entityBuilder()
+    public Entity createEntity() {
+        entity = FXGL.entityBuilder()
                 .at(400, 200)
                 .viewWithBBox(new Circle(5, Color.BLACK))
                 .with(new CollidableComponent(true))
                 .type(EntityTypes.BOSS)
                 .buildAndAttach();
-        return entityBuilder;
-    }
-
-    public HP getHP() {
-        return hp;
+        return entity;
     }
 }
